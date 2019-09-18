c41ade96faefeaa8f4432c760de64f24e4e12df8
Leonardo Salema <leosalema@yahoo.com.br>
2019-09-18 16:03:16 -0300

    ajuste nos campos a serem salvos

--- src/app.js
+++ src/app.js
@@ -15,8 +15,23 @@ db.once('open', function(callback) {
 })
 
 const discordSchema = new mongoose.Schema({
-  author: String,
+  channelType: String,
+  channelId: String,
+  channelName: String,
+  channelPosition: Number,
+  channelParentID: String,
+  channelLastMessageID: String,
+  guildName: String,
+  guildSysyemChangelID: String,
+  guildId: String,
+  guildOwnerId: String,
+  id: String,
+  type: String,
   content: String,
+  authorId: String,
+  authorUsername: String,
+  authorBot: Boolean,
+  readyAt: Date,
   createdAt: {type: Date, default: Date.now}
 });
 
@@ -28,14 +43,27 @@ client.on('ready', () => {
 
 client.on('message', message => {
   let discord = new Discord({
-    author: message.author.username,
-    content: message.content
+    channelType: message.channel.type,
+    channelId: message.channel.id,
+    channelName: message.channel.name,
+    channelPosition: message.channel.position,
+    channelParentID: message.channel.parentID,
+    channelLastMessageID: message.channel.lastMessageID,
+    guildName: message.channel.guild.name,
+    guildSysyemChangelID: message.channel.guild.systemChannelID,
+    guildId: message.channel.guild.id,
+    guildOwnerId: message.channel.guild.ownerID,
+    id: message.id,
+    type: message.type,
+    content: message.content,
+    authorId: message.author.id,
+    authorUsername: message.author.username,
+    authorBot: message.author.bot,
+    readyAt: message.mentions._client.readyAt,
   })
 
   discord.save(function(error) {
-    if (error) {
-      console.log(error);
-    }
+    if (error) console.log(error);
   })
 });
 
